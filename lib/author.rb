877bddb022724b2ca937eea72c369de299d77e1d
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 11:30:35 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,8 +14,8 @@ class Author
     @@post_count +=1
   end
 
-  def add_post_by_title(title, author)
-    post = Post.new(title, author)
+  def add_post_by_title(title)
+    post = Post.new(title)
     @posts << post
     post.author = self
     @@post_count +=1
