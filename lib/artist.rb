34ad6001190e719f12734d98519dc3a229e976c6
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 10:54:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,11 +3,10 @@ class Artist
   
   @@song_count = 0
   
-  def initialize(name)
+   def initialize(name)
     @name = name
     @songs = []
   end
-  
 def add_song(song)
     self.songs << song
     song.artist = self
