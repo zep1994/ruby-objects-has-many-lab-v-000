43f6e3453f9d6b85432d8ca95280e1e40fb14228
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 10:56:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,14 @@
 class Artist
-   attr_accessor :name
+  attr_accessor :name
 
   @@song_count = 0
-  
-   def initialize(name)
+
+  def initialize(name)
     @name = name
     @songs = []
   end
-  
- def add_song(song)
+
+  def add_song(song)
     self.songs << song
     song.artist = self
     @@song_count +=1
@@ -21,8 +21,11 @@ class Artist
     @@song_count +=1
   end
 
-   def songs
-    @songs
+  def songs
+    @songs 
   end
-  
+
+  def self.song_count
+    @@song_count
+  end
 end
