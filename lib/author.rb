de96eaf2786447c31847954aa43bbc76b89dc3df
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 11:04:42 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,23 +1,31 @@
 class Author
-  attr_accessor :name, :posts 
+  attr_accessor :name, :posts
 
-  @@all = []
+  @@post_count = 0
 
-  def initialize(name, artist=nil)
+  def initialize(name)
     @name = name
-    @artist = artist
-    @@all << self
+    @songs = []
   end
 
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count +=1
   end
 
-  def self.all
-    @@all
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
+  end
+
+  def self.song_count
+    @@song_count
   end
 end
